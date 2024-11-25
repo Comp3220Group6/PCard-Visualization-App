fbbb0f5bb21401ca8c5333c1cf2e1db025ddc07c
kkewa1 <kewaquok@uwindsor.ca>
2024-11-24 23:39:12 -0500

    Update ExcelList.java
    better info

--- test/ExcelList.java
+++ test/ExcelList.java
@@ -2,13 +2,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-//This class contains the excel info in a list
+//This class contains the excel files in a list
 public class ExcelList {
     public ArrayList<Excel> excelList = new ArrayList<Excel>();
 
     public ExcelList() throws IOException {
         //specific path for files
-        File folder = new File("./expenditures/2013");
+        File folder = new File("./expenditures/2013");//currently set to 2013 (will need to be changed to work with any/filter)
         File[] listOfFiles = folder.listFiles();
         //loops through files and adds them to the list
         if(listOfFiles != null) {
